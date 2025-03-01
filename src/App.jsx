0c88a3f180512136a756b9f90d47acba59f3153d
Lidia Glez-Tablas <lgtablasf5@gmail.com>
2025-03-01 23:56:38 +0100

    CSS stable, correct layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import Footer from "./components/footer/footer.jsx"
 
 import Recordings from './components/pages/recordings/recordings.jsx'
 import "./app.css"
+import { HomeIcon } from "lucide-react"
 
 
 
 
 const App = () => {
-
+ 
   return (
+    <icon icon_home = {{fill:'blue'}}/>,
+    <icon icon_menu = {{fill:'blue'}}/>,
+    <icon play-button ={{fill:'green'}}/>,
+
     <div>
     <Header />
     <Recordings/>

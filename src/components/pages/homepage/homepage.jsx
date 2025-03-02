364c838b672a50a14357dee4cd7bb0c8b60049ed
Carlotaml21 <carlotaml21@gmail.com>
2025-03-02 22:24:45 +0100

    add piano img edit css

--- src/components/pages/homepage/homepage.jsx
+++ src/components/pages/homepage/homepage.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import "./homepage.css"
-
+import "./homepage.css";
+import piano from "../../../assets/homeImages/MIDI_DosOctavas.jpg";
 
 const HomePage = () => {
-
-    return(
+    return (
         <div className="home_container">
             <h1 className="home_title">Descubre y toca instrumentos de manera interactiva</h1>
-            <div className="home_images">
-                <a href=""><img src="" alt="" className="home_image"></img></a>
-                <img src="" alt="" className="home_image"></img>
+            <div className="homeImages">
+                <div className="instrument">
+                    <img src={piano} alt="Piano" className="home_image" />
+                    <p className="instrument_text">Piano</p>
+                </div>
+                <div className="instrument">
+                    <img src={piano} alt="Tambor" className="home_image" />
+                    <p className="instrument_text">Tambor</p>
+                </div>
             </div>
         </div>
     );
